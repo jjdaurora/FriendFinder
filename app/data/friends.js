260ff4dba865499141dc48ea697fc93501bfa5f4
JJ D'Aurora <jjdaurora@gmail.com>
2018-01-28 15:54:59 -0500

    interface complete - routing files complete - npm dependencies installed - package.json

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+  {
+    name: "James Garfield",
+    email: "jgarfield@presidents.com",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Cester Arthur",
+    email:"carthur@presidents.com",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Martin Van Buren",
+    email: "mvanburen@presidents.com",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Milliard Filmore",
+    email: "mfilmore@presidents.com",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Franklin Pierce",
+    email: "fpierece@presidents.com",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Zachary Taylor",
+    email: "ztaylor@presidents.com",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  },
+  {
+    name: "Benjmain Harrison",
+    email: "bharrison@presidents.com",
+    scores: ["3", "3", "3", "4", "3", "3", "3", "3", "3", "2"]
+  }
+];
