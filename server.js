0e3d9a39d7d5ee49c332e3ab653ec0e7905140a3
JJ D'Aurora <jjdaurora@gmail.com>
2018-01-31 01:21:45 -0500

    app created

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ var apiRoutes = require("/app/routing/apiRoutes")(app);
 var apiRoutes = require("/app/routing/htmlRoutes")(app);
 
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function (newFriend) {
+function (newFriend) { 
 
 }
 
 
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log("App listening on PORT " + PORT);
 });
